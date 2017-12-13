0cdf9837108b5234c66f6e3953a07e59d74233db
Kevin Morelli <kev9318@gmail.com>
2017-12-12 23:03:04 -0300

    terminado los data access

--- TP WebApp/Data/DataAccess/ShiftsData.cs
+++ TP WebApp/Data/DataAccess/ShiftsData.cs
@@ -10,18 +10,35 @@ namespace Data.DataAccess
 {
     public class ShiftsData
     {
+        private Repository<Shift> Repository;
+
+        public ShiftsData()
+        {
+            this.Repository = new Repository<Shift>();
+        }
+
         public List<Employee> GetEmployees(int id)
         {
             using (var context = new Context())
             {
                 return context.Shifts
                     .Include(c => c.Employees)
+                    .AsNoTracking()
                     .Where(c => c.ID == id)
                     .FirstOrDefault()
                     .Employees;
             }
         }
+        
+        public List<Shift> ReadAll()
+        {
+            return this.Repository.Set().ToList();
+        }
 
+        public Shift Read(int id)
+        {
+            return this.Repository.GetById(id);
+        }
         
     }
 }

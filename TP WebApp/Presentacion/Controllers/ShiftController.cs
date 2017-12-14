976ad5e107f7f44694744e631af4f484dcf4dbfe
Kevin Morelli <kev9318@gmail.com>
2017-12-14 14:02:05 -0300

    seteada la vista shifts para registro de horarios y cobranzas

---
+++ TP WebApp/Presentacion/Controllers/ShiftController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Presentacion.Controllers
+{
+    public class ShiftController : Controller
+    {
+        // Read shifts
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //Menu de registro de horarios
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        //Menu de cobrar
+        public ActionResult Cash()
+        {
+            
+            return View();
+        }
+    }
+}

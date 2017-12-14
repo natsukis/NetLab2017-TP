8ede25b8eae074a10bd1ef18e9ce900a9143cf42
LAPTOP-H9L2EKRH\janil <dantasjanile@gmail.com>
2017-12-14 01:08:07 -0600

    CRUD Empleados - Inicio

---
+++ TP WebApp/Presentacion/Controllers/EmployeeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Presentacion.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private CrudEmployee crudEmployee;
+
+        public EmployeeController(CrudEmployee crudEmployee)
+        {
+            this.crudEmployee = crudEmployee;
+        }
+        public ActionResult Form()
+        {
+            return View();
+        }
+
+        public ActionResult Add(EmployeeModel employee)
+        {
+            if()
+        }
+    }
+}

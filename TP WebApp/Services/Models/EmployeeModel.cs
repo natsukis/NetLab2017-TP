08e6ddb089bd15780f676b9e193e1be3edcfc6e9
JanileDantas <dantasjanile@gmail.com>
2017-12-17 22:04:25 -0600

    Implementando el ModelValidator de la presentacion del crudEmployees

--- TP WebApp/Services/Models/EmployeeModel.cs
+++ TP WebApp/Services/Models/EmployeeModel.cs
@@ -1,6 +1,7 @@
 ﻿using Services.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,10 +12,10 @@ namespace Services
     {
         public int ID { get; set; }
 
-  
+        [Required, StringLength(50)]
         public string FirstName { get; set; }
 
-
+        [Required, StringLength(50)]
         public string LastName { get; set; }
 
   
